7484f0d1f2902c2ea17f26e1be76cd6c167997e6
Jayprakash Sahu <118419073+Jpfpp12@users.noreply.github.com>
2025-04-30 18:28:07 +0530

    Create data.js

---
+++ src/components/data.js
@@ -0,0 +1,46 @@
+const menu = [
+  {
+    id: 1,
+    title: "Pancakes",
+    category: "breakfast",
+    price: "$5.99",
+    img: "https://via.placeholder.com/150",
+  },
+  {
+    id: 2,
+    title: "Burger",
+    category: "lunch",
+    price: "$8.99",
+    img: "https://via.placeholder.com/150",
+  },
+  {
+    id: 3,
+    title: "Milkshake",
+    category: "shakes",
+    price: "$4.99",
+    img: "https://via.placeholder.com/150",
+  },
+  {
+    id: 4,
+    title: "Eggs Benedict",
+    category: "breakfast",
+    price: "$6.99",
+    img: "https://via.placeholder.com/150",
+  },
+  {
+    id: 5,
+    title: "Grilled Cheese",
+    category: "lunch",
+    price: "$7.99",
+    img: "https://via.placeholder.com/150",
+  },
+  {
+    id: 6,
+    title: "Strawberry Shake",
+    category: "shakes",
+    price: "$5.49",
+    img: "https://via.placeholder.com/150",
+  },
+];
+
+export default menu;
